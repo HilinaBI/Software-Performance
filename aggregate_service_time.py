19325f33752214d01b470f28fee83c74a7307844
KyleJYKim <kimjuyoung656@icloud.com>
2025-06-14 22:11:47 +0200

    service time update

--- aggregate_service_time.py
+++ aggregate_service_time.py
@@ -7,26 +7,26 @@ from starlette.middleware.base import BaseHTTPMiddleware
 log_file = "service_time.log"
 
 # Regex to extract the individual service time
-service_pattern = re.compile(r"\[SERVICE TIME\] ([\d.]+)s")
-render_pattern = re.compile(r"\[RENDER TIME\] ([\d.]+)s")
-tot_req_pattern = re.compile(r"\[TOTAL REQUEST TIME\] ([\d.]+)s")
+service_pattern = re.compile(r"\[TOTAL SERVICE TIME\] ([\d.]+)s")
+server_pattern = re.compile(r"\[SERVER TIME\] ([\d.]+)s")
+db_pattern = re.compile(r"\[DB TIME\] ([\d.]+)s")
 
 service_times = []
-render_times = []
-tot_req_times = []
+server_times = []
+db_times = []
 
 # Read and extract all service time values
 with open(log_file, "r") as f:
     for line in f:
         service_match = service_pattern.search(line)
-        render_match = render_pattern.search(line)
-        tot_req_match = tot_req_pattern.search(line)
+        server_match = server_pattern.search(line)
+        db_match = db_pattern.search(line)
         if service_match:
             service_times.append(float(service_match.group(1)))
-        elif render_match:
-            render_times.append(float(render_match.group(1)))
-        elif tot_req_match:
-            tot_req_times.append(float(tot_req_match.group(1)))
+        elif server_match:
+            server_times.append(float(server_match.group(1)))
+        elif db_match:
+            db_times.append(float(db_match.group(1)))
 
 
 # Compute final average
@@ -40,16 +40,16 @@ if service_times:
 else:
     print("No service times found.")
     
-if render_times:
-    avg_render_time = sum(render_times) / len(render_times)
-    print(f"Final average render time: {avg_render_time:.10f} seconds over {len(render_times)} requests")
+if server_times:
+    avg_server_time = sum(server_times) / len(server_times)
+    print(f"Final average server time: {avg_server_time:.10f} seconds over {len(server_times)} requests")
 else:
-    print("No render times found.")
+    print("No server times found.")
     
-if tot_req_times:
-    avg_tot_req_time = sum(tot_req_times) / len(tot_req_times)
-    print(f"Final average total request time: {avg_tot_req_time:.10f} seconds over {len(tot_req_times)} requests")
+if db_times:
+    avg_db_time = sum(db_times) / len(db_times)
+    print(f"Final average db time: {avg_db_time:.10f} seconds over {len(db_times)} requests")
 else:
-    print("No total request times found.")
+    print("No db times found.")
     
     
